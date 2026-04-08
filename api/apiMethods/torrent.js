6e337b9f64f1b85543484a3dd91080c9f3ec51e4
scott smith <scottsmith75954@gmail.com>
2026-04-08 11:09:45 +0000

    Can now find, remove, pause and resume torrents

--- api/apiMethods/torrent.js
+++ api/apiMethods/torrent.js
@@ -18,7 +18,7 @@ app.get('/torrentSearch', async (req, res) => {
     console.log("query: ",query);
 
     // make call to prowlarr
-    const search = new URL('http://cupboard:9696/api/v1/search?query='+query+'&categories=2000');
+    const search = new URL(process.env.PROWLARR_URL + 'api/v1/search?query='+query+'&categories=2000');
     fetch(search, {
         headers: {
             'X-Api-Key':process.env.PROWLARR_KEY
@@ -45,26 +45,32 @@ app.get('/auth',upload.none(), async (req, res) => {
     body.append("username", process.env.QBIT_USERNAME);
     body.append("password", process.env.QBIT_PASSWORD);
 
-    const url = 'http://cupboard:8080/api/v2/auth/login';
+    // const url = 'http://cupboard:8080/api/v2/auth/login';
+    const url = process.env.QBit_URL + "api/v2/auth/login";
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: body.toString()
-    });
+    try {        
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: body.toString()
+        });
+    //SOMTHING WRONG HERE \\
+        console.log("AuthResults");
+        QBITCookie = response.headers.get('set-cookie');
+        // QBITCookie = response.headers.get('set-cookie').split(';')[0].slice(8);
+        console.log("QBITCookie: ", QBITCookie);
+        
+        res.send(response);
 
-    console.log("AuthResults");
-    QBITCookie = response.headers.get('set-cookie');
-    // QBITCookie = response.headers.get('set-cookie').split(';')[0].slice(8);
-    console.log("QBITCookie: ", QBITCookie);
-    
-    res.send(response);
+    } catch (error) {
+        res.send(error);
+    }
 
 });
 
-//adding new user to the db
+//adding new torrent
 app.get('/addTorrent',upload.none(), async (req, res) => {
     const magnet = req.query.magnet;
 
@@ -75,7 +81,8 @@ app.get('/addTorrent',upload.none(), async (req, res) => {
     console.log("QBITCookie Provided " , QBITCookie);
 
     // send magnet to qbittorrent
-    const request = new URL('http://cupboard:8080/api/v2/torrents/add');
+    // const request = new URL(process.env.QBit_URL + "http://cupboard:8080/api/v2/torrents/add');
+    const request = new URL(process.env.QBit_URL + "api/v2/torrents/add");
     fetch(request, {
         method: "POST",
         body: formData,
@@ -93,6 +100,120 @@ app.get('/addTorrent',upload.none(), async (req, res) => {
     .catch(error => {
         console.error("couldnt add magnet", error);
         // res.send("couldnt add magnet", error);
+    });
+
+});
+
+//gets all the data relating to all downloading torrents
+app.get('/getTorrents',upload.none(), async (req, res) => {
+    // send magnet to qbittorrent
+    const request = new URL(process.env.QBit_URL + "api/v2/torrents/info");
+    fetch(request, {
+        method: "POST",
+        headers:{
+            "Cookie": QBITCookie
+        }
+    })
+    .then(response => response.text())
+    .then(data =>{
+        // console.log("response...");
+        // console.log(data);
+
+        res.send(data);
+    })
+    .catch(error => {
+        console.error("couldnt add magnet", error);
+    });
+
+});
+
+//removing passed torrent from qbit 
+app.get('/removeTorrent',upload.none(), async (req, res) => {
+    const hash = req.query.hash;
+
+    console.log("attemping to remove " , hash);
+
+    const url = new URL(process.env.QBit_URL + "api/v2/torrents/delete");
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": QBITCookie
+        },
+        body: new URLSearchParams({
+            hashes: hash,
+            deleteFiles: "true"
+        })
+    })
+    .then(response => response.text())
+    .then(data =>{
+        console.log("removed torrent: " , hash);
+        console.log(data);
+
+        res.send(data);
+    })
+    .catch(error => {
+        console.error("couldnt remove torrent: ", error);
+    });
+
+});
+
+//pausing a passed torrent from qbit 
+app.get('/pauseTorrent',upload.none(), async (req, res) => {
+    const hash = req.query.hash;
+
+    console.log("attemping to remove " , hash);
+
+    const url = new URL(process.env.QBit_URL + "api/v2/torrents/pause");
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": QBITCookie
+        },
+        body: new URLSearchParams({
+            hashes: hash,
+        })
+    })
+    .then(response => response.text())
+    .then(data =>{
+        console.log("paused torrent: " , hash);
+        console.log(data);
+
+        res.send(data);
+    })
+    .catch(error => {
+        console.error("couldnt pause torrent: ", error);
+    });
+
+});
+
+//pausing a passed torrent from qbit 
+app.get('/resumeTorrent',upload.none(), async (req, res) => {
+    const hash = req.query.hash;
+
+    console.log("attemping to resume " , hash);
+
+    const url = new URL(process.env.QBit_URL + "api/v2/torrents/resume");
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": QBITCookie
+        },
+        body: new URLSearchParams({
+            hashes: hash,
+        })
+    })
+    .then(response => response.text())
+    .then(data =>{
+        console.log("resumed torrent: " , hash);
+        console.log(data);
+
+        res.send(data);
+    })
+    .catch(error => {
+        console.error("couldnt resume torrent: ", error);
     });
 
 });
--- api/website/scripts/torrentSearch.js
+++ api/website/scripts/torrentSearch.js
@@ -17,6 +17,8 @@ function onload(){
     fetch(url+"auth")
     .then(response => response.text())
     .then(data => {
+        console.log("result from auth", data);
+
         if (data == "Fails."){
             document.getElementById("error").innerText = "Failed to connect to QBIT client";
         }else if(data == "Your IP address has been banned after too many failed authentication attempts."){
@@ -26,6 +28,8 @@ function onload(){
     .catch(error => {
         console.error("couldnt reach torrent client", error);
     });
+
+    checkDownloads();
 }
 
 //searches for torrents bassed off input from user
@@ -38,6 +42,7 @@ function search(){
     fetch(search)
     .then(response => response.json())
     .then(data =>{
+            document.getElementById("searchResults").innerHTML = "";
             console.log("search results");
             console.log(data);
             
@@ -68,7 +73,7 @@ function makeWidget(name, magnet, seeders){
     
     const contentS = document.createElement("p");
     contentS.className = "seeders"; 
-    contentS.innerText = "seeders: " , seeders;
+    contentS.innerText = "seeders :" + seeders;
     contentS.id = magnet;
 
     const button = document.createElement("button");
@@ -94,24 +99,116 @@ function makeWidget(name, magnet, seeders){
     main.appendChild(div);
 }
 
-//list all 
-listQuery => function(response){
-    console.log(response);
+//pulls down all current downloads every x
+const interval = setInterval(checkDownloads, 5000);
+//checks the status of all current torrents.
+function checkDownloads(){
 
-}
-
-// get updates on torrents
-// get an update on currently downloading torrents every 10 secounds 
-function getUpdate(){
-
-
-    fetch(url+"addTorrent", {
-        method: "POST",
-        body: formData,
-    })
+    fetch(url+"getTorrents")
     .then(response => response.json())
-    .then(listQuery)
+    .then(data => {
+        console.log(data);
+        maindiv = document.getElementById("downloading").innerHTML = "";
+
+        data.forEach(item => {
+            makeTorrentWidget(item.name, item.num_seeds, item.eta, item.dlspeed,item.state,item.hash);
+        });
+    })
     .catch(error => {
         console.error("couldnt reach torrent client", error);
     });
+}
+
+function makeTorrentWidget(name, seeders, eta, dlspeed,state,hash){
+    maindiv = document.getElementById("downloading");
+
+    if(state == "stalledUP" || state == "pausedUP"){
+        eta = 0;
+    }
+
+    const torrentDiv = document.createElement("div");
+    torrentDiv.className = "TorrentDiv";
+    torrentDiv.id = "TorrentDiv";
+
+    const contentName = document.createElement("p");
+    contentName.className = "torrentName"; 
+    contentName.innerText = name;
+
+    const contentSeeders = document.createElement("p");
+    contentSeeders.className = "torrentSeeders"; 
+    contentSeeders.innerText = "Seeders : " + seeders;
+
+    const contentEta = document.createElement("p");
+    contentEta.className = "torrentEta"; 
+    contentEta.innerText = "ETA : " + Math.round(eta/60) + " Minutes";
+    
+    const contentSpeed = document.createElement("p");
+    contentSpeed.className = "torrentDlspeed"; 
+    contentSpeed.innerText = "Download-Speed : " + Math.round(dlspeed/1000000) + " Mib/s";
+
+    const contentState = document.createElement("p");
+    contentState.className = "torrentState"; 
+    contentState.innerText = "State : " + state;
+
+    const buttonRemove = document.createElement("button");
+    buttonRemove.innerHTML = "Remove"
+    buttonRemove.className = "torrentButton";
+    buttonRemove.id = "removeTorrentButton";
+    buttonRemove.onclick = () =>{ //removes this torrent and deletes any files
+        const search = new URL(url+"removeTorrent");
+        search.searchParams.append("hash",hash);
+        
+        fetch(search)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("couldnt reach torrent client", error);
+        });
+    }
+
+    const buttonPause = document.createElement("button");
+    buttonPause.innerHTML = "Pause"
+    buttonPause.className = "torrentButton";
+    buttonPause.id = "pauseTorrentButton";
+    buttonPause.onclick = () =>{ //Pauses this torrent
+        if(state == "pausedDL"){
+            const search = new URL(url+"resumeTorrent");
+            search.searchParams.append("hash",hash);
+            
+            fetch(search)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("couldnt reach torrent client", error);
+            });
+        }else{
+
+            const search = new URL(url+"pauseTorrent");
+            search.searchParams.append("hash",hash);
+            
+            fetch(search)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("couldnt reach torrent client", error);
+            });
+        }
+        
+    }
+
+    torrentDiv.appendChild(contentName);
+    torrentDiv.appendChild(contentSeeders);
+    torrentDiv.appendChild(contentEta);
+    torrentDiv.appendChild(contentSpeed);
+    torrentDiv.appendChild(contentState);
+    torrentDiv.appendChild(buttonRemove);
+    torrentDiv.appendChild(buttonPause);
+    maindiv.appendChild(torrentDiv);
+
 }
